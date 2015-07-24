f69d49eb0662b765d00b6c501646f182fbbf350e
Tony Destro <tony.destro@gmail.com>
2015-07-24 14:04:02 -0400

    Revert "Merge branch 'pr/5'"
    This reverts commit 9e41711944537ef7a592b251956f5f2793c3828d, reversing
    changes made to 02ce3ee3696f49fdf28ac9aa5188f949bfbd5798.

--- aws/aws.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-VBoxmanage export gltc_default_1436816400578_33919 --output gltc_default_1436816400578_33919.ovf
-ec2-import-instance gltc_default_1436816400578_33919-disk1.vmdk -f VMDK -t m1.small -a x86_64 -b gltc -o $AWS_ACCESS_KEY -w $AWS_SECRET_KEY --region us-west-2
-ec2-describe-conversion-tasks import-i-fg63jjs9 --region us-west-2
